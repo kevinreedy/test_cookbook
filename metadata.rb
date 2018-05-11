92b3592713a454b732f8d9053ed1069c40125ef8
Kevin Reedy <kevinreedy@gmail.com>
2018-05-11 10:32:32 -0500

    hello chef workstation

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'kevinreedy@gmail.com'
 license 'all_rights'
 description 'Installs/Configures test_cookbook'
 long_description 'Installs/Configures test_cookbook'
-version '0.1.4'
+version '0.2.0'
 
 issues_url 'https://git.dayold.pizza/kreedy/test_cookbook/issues' if respond_to?(:issues_url)
 source_url 'https://git.dayold.pizza/kreedy/test_cookbook' if respond_to?(:source_url)
