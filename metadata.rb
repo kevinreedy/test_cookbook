ab51f7d5daacffb6ca89eb7b2d746791efe8db3d
Kevin Reedy <kevinreedy@gmail.com>
2018-05-23 11:19:24 -0500

    a2

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'kevinreedy@gmail.com'
 license 'all_rights'
 description 'Installs/Configures test_cookbook'
 long_description 'Installs/Configures test_cookbook'
-version '0.2.0'
+version '0.2.1'
 
 issues_url 'https://git.dayold.pizza/kreedy/test_cookbook/issues' if respond_to?(:issues_url)
 source_url 'https://git.dayold.pizza/kreedy/test_cookbook' if respond_to?(:source_url)
--- recipes/default.rb
+++ recipes/default.rb
@@ -5,5 +5,5 @@
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
 file '/tmp/hello.txt' do
-  content 'Hello, Chef Workstation!'
+  content 'Hello, Chef Automate 2!'
 end
